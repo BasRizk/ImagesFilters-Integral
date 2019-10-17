f618e0b0a6458b8aa40a4b48d102d1c6b4b6ea3b
Basem Rizk <basem.rizk@outlook.com>
2019-10-17 18:43:45 +0200

    complete documentation and answer problem 2c

--- CV_Ass.py
+++ CV_Ass.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
+#
+#
+# Authors: Ibram Medhat, Basem Rizk
+#
+# Assignment 1
+# DMET901 - Computer Vision 
+# The German University Of Cairo
+#
 
 import numpy as np
-#import matplotlib.pyplot as plt
-#import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 from PIL import Image
 
+# =============================================================================
 #Function takes as arguments gray scale image and A,B,C,D to determine the contrast increase applied
 #returns the filtered image
+# =============================================================================
+
 def increase_contrast(gray_img, A, B, C, D):
     width, height = gray_img.size
 #    gray_scale_contrast = 0;
@@ -21,9 +31,11 @@ def increase_contrast(gray_img, A, B, C, D):
     
     return gray_img
 
+# =============================================================================
 # Function takes as input the gray_img and compute the contrast using 
 # cooccurance matrix with north-south relation
 # between pixels and returns the contrast value of the given image 
+# =============================================================================
 def compute_contrast(gray_img):
     width, height = gray_img.size
     gray_scale_contrast = 0;
@@ -32,6 +44,10 @@ def compute_contrast(gray_img):
             gray_scale_contrast += (gray_img.getpixel((i, j)) - gray_img.getpixel((i, j + 1))) ** 2
     return gray_scale_contrast
 
+# =============================================================================
+# Function takes as input the a numpy array representing an original grayscale image
+# @returns the corresponding integral image array representation
+# =============================================================================
 def compute_integral_image(org_img_array):
 #    org_img_array = np.array(gray_img)
     int_img_array = np.zeros([org_img_array.shape[0], org_img_array.shape[1]], dtype=int)
@@ -49,6 +65,11 @@ def compute_integral_image(org_img_array):
     
     return int_img_array
 
+# =============================================================================
+# Function takes as input a numpy integral image array,
+# @returns a tuple containing
+# (numpy array of the filtered image, filtered image as Pillow instance)
+# =============================================================================
 def average_filter_using_int_image_array(int_img_array, filter_size = 3): 
     print("About to apply averaging filter, using Integral Image computation")
     print(int_img_array)
@@ -70,6 +91,22 @@ def average_filter_using_int_image_array(int_img_array, filter_size = 3):
     filtered_img = Image.fromarray(filtered_img_array)
     filtered_img = filtered_img.convert("L")
     return filtered_img_array, filtered_img
+
+# =============================================================================
+#                           Problem 2 :: C
+# ============================================================================= 
+# The Function average_filter_using_int_image_array uses the integral image
+# representation of an image to compute a filtered version of it.
+#
+# The advantage of using the integral image for average filtering here compared
+# to traditional convolution implementation of average filtering is :
+# 
+# that it does not need to add up all the values of pixels lying underneath
+# the area of the filter; however, takes advantage of the cummulation of values
+# produced already in the integral image representation
+# ; hence, less computation.
+#
+# =============================================================================
 
 # =============================================================================
 # Initializations
